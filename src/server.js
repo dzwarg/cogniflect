74788326645a6e9d1ab39641ff266e9733e76209
David Zwarg <dzwarg@massmutual.com>
2018-09-17 16:16:35 +0000

    Merged in sockets (pull request #1)
    Sockets
    
    * Add socket server.
    
        Begin experimenting with a socket server for multi-clients.
    
    * Configured socket properly.

---
+++ src/server.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var port = 5000;
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
